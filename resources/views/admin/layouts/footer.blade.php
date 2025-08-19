9b9ed2f7bfc0d94ec3079f1784e24ec10d40d50c
Putri Yogi Noviana <l200214232@student.ums.ac.id>
2025-08-19 18:50:31 +0700

    initial commit

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<script src="/vendor/admin/plugins/jquery/jquery.min.js"></script>
+<script src="/vendor/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/vendor/admin/dist/js/adminlte.min.js"></script>
+</body>
+</html>
---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,75 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="/" class="brand-link">
+  <span class="brand-text font-weight-light">Kasir</span>
+</a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+     
+
+
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+         
+          {{-- <li class="nav-item ">
+            <a href="/" class="nav-link {{ Request::is('/') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li> --}}
+
+          <li class="nav-item">
+            <a href="/admin/transaksi" class="nav-link {{ Request::is('admin/transaksi*') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-exchange-alt"></i>
+              <p>
+                Transaksi
+              </p>
+            </a>
+          </li>
+
+          
+
+          <li class="nav-item">
+            <a href="/admin/produk" class="nav-link {{ Request::is('admin/produk*') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Produk
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="/admin/kategori" class="nav-link {{ Request::is('admin/kategori*') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-list"></i>
+              <p>
+                Kategori
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="/admin/user" class="nav-link {{ Request::is('admin/user*') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-users"></i>
+              <p>
+                User
+              </p>
+            </a>
+          </li>
+
+
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <div class="content-wrapper">
